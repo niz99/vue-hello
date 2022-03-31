2739f2988edb386a7723ab4b36ced1cbcc309ac1
Nizar Fergag <fergag99@gmail.com>
2022-03-31 09:47:49 +0200

    esercizio completato

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            title: 'Titolo scritto con Vue',
+            image: 'https://source.unsplash.com/random'
+        }
+    },
+);
